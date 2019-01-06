1323aa393aece941f284da2ab85b6a02f4b68b10
selim13 <selim013@gmail.com>
2019-01-06 17:41:42 +0300

    refactor: move datauri's traversing to separate function

--- src/index.js
+++ src/index.js
@@ -11,37 +11,58 @@ const input = document.querySelector('.css-input');
 const results = document.querySelector('.results');
 const zipBtn = document.querySelector('.zip');
 
-document.addEventListener('input', e => handleInput(e.target.value));
+input.addEventListener('input', e => handleInput(e.target.value));
+handleInput(input.value);
 
-function decode(dataURI) {
-  const matches = dataURI.match(/data:(image\/.+);base64,(.+)/);
-  return {
-    type: matches[1],
-    data: new Buffer(matches[2], 'base64')
-  };
+function getURIs(code) {
+  const pattern = /url\(['"]?(data:(.+?)(?:;.+?)?(;base64)?,(.+))['"]?\)/gi;
+
+  const result = [];
+  let match;
+  while ((match = pattern.exec(code))) {
+    result.push({
+      uri: match[1],
+      mediaType: match[2],
+      isBase64: match[3] ? true : false,
+      data: match[4]
+    });
+  }
+
+  return result;
 }
 
 function handleInput(value) {
   results.innerHTML = '';
+
+  const files = getURIs(value);
   const zip = new JSZip();
 
-  const pattern = /url\(['"]?(.+)['"]?\)/g;
-  let match;
-  while ((match = pattern.exec(value))) {
-    const dataBase64 = match[1];
-    const dataHash = hex.stringify(md5(dataBase64));
-    const decoded = decode(dataBase64);
-    const extension = mimeTypes.extension(decoded.type);
-    const blob = new Blob([decoded.data], { type: decoded.type });
-    const fileName = `${dataHash}.${extension}`;
+  files.forEach(file => {
+    const data = file.isBase64 ? new Buffer(file.data, 'base64') : file.data;
+    const extension = mimeTypes.extension(file.mediaType)
+      ? mimeTypes.extension(file.mediaType)
+      : 'bin';
+    const hash = hex.stringify(md5(data));
+    const blob = new Blob([data], { type: file.mediaType });
+    const fileName = `${hash}.${extension}`;
 
-    zip.file(`dataurl/${fileName}`, decoded.data);
+    zip.file(`dataurl/${fileName}`, data);
+
+    let img =
+      "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24'%3E%3Cpath d='M5 20h14v-2H5m14-9h-4V3H9v6H5l7 7 7-7z'/%3E%3C/svg%3E";
+
+    if (['woff', 'woff2', 'ttf', 'eot'].includes(extension))
+      img =
+        "data:image/svg+xml,%3Csvg baseProfile='tiny' xmlns='http://www.w3.org/2000/svg' width='24' height='24'%3E%3Cpath fill='none' d='M0 0h24v24H0V0z'/%3E%3Cpath d='M9.93 13.5h4.14L12 7.98zM20 2H4c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-4.05 16.5l-1.14-3H9.17l-1.12 3H5.96l5.11-13h1.86l5.11 13h-2.09z'/%3E%3C/svg%3E";
+
+    if (['png', 'jpg', 'jpeg', 'gif', 'bmp', 'webp', 'svg'].includes(extension))
+      img = file.uri;
 
     const figure = document.createElement('figure');
     figure.classList.add('results__item');
     figure.insertAdjacentHTML(
       'beforeend',
-      `<img src="${dataBase64}" alt="" title="${fileName}">
+      `<img src="${img}" alt="" title="${fileName}">
     <div class="results__item-type">${extension}</div>
     `
     );
@@ -49,7 +70,7 @@ function handleInput(value) {
     figure.addEventListener('click', () => saveAs(blob, fileName));
 
     results.appendChild(figure);
-  }
+  });
 
   zipBtn.addEventListener('click', () => {
     zip.generateAsync({ type: 'blob' }).then(function(blob) {
