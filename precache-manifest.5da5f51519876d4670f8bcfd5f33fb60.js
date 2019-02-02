76446465b6f8f150de88fcc93e201910810af23a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-02 16:27:12 +0000

    Deploy selim13/datauri-extractor-app to github.com/selim13/datauri-extractor-app.git:gh-pages

---
+++ precache-manifest.5da5f51519876d4670f8bcfd5f33fb60.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8fedfbbfded528777d79d3d856d0e39",
+    "url": "mstile-144x144.png"
+  },
+  {
+    "revision": "21dc7045992984f4dcf5",
+    "url": "main.f52661c9.css"
+  },
+  {
+    "revision": "f2ce80947516ab6b97f9a48bf36c25fc",
+    "url": "safari-pinned-tab.svg"
+  },
+  {
+    "revision": "782125ba9a1672c898468fc48673bb2b",
+    "url": "mstile-310x310.png"
+  },
+  {
+    "revision": "cbb4a3ad0443b83161d6247512f73ef4",
+    "url": "site.webmanifest"
+  },
+  {
+    "revision": "ef2775e0eb36e8c55ace39aff0d4d6b5",
+    "url": "mstile-70x70.png"
+  },
+  {
+    "revision": "3161e8d888e3af35c8edabc39af01f14",
+    "url": "mstile-310x150.png"
+  },
+  {
+    "revision": "e4858ff182f97d37511273988486f694",
+    "url": "mstile-150x150.png"
+  },
+  {
+    "revision": "21dc7045992984f4dcf5",
+    "url": "main.d6209245.bundle.js"
+  },
+  {
+    "revision": "cd21faa4c870716940f283f077b0325e",
+    "url": "index.html"
+  },
+  {
+    "revision": "e0b09b0f66229a37b4a182048467eeae",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "1cc4b2c8c729161fcfab1974d8fb8b8c",
+    "url": "favicon-32x32.png"
+  },
+  {
+    "revision": "9041cc5005a609fad4518558912ebb85",
+    "url": "favicon-16x16.png"
+  },
+  {
+    "revision": "0af73f645d540446ab2db6b588593e9f",
+    "url": "browserconfig.xml"
+  },
+  {
+    "revision": "fce11976ba48defa513a7af10cbce13f",
+    "url": "apple-touch-icon.png"
+  },
+  {
+    "revision": "c3590fbee4b2a912dc412a71eb268408",
+    "url": "android-chrome-512x512.png"
+  },
+  {
+    "revision": "f1087fb4749cd864e4fefca6b28dea31",
+    "url": "android-chrome-192x192.png"
+  }
+];
