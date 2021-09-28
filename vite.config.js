df10c27ea3f39c5b7f59359955649a5beaa44019
selim13 <selim013@gmail.com>
2021-09-28 21:20:08 +0300

    add compression plugin

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
+import viteCompression from 'vite-plugin-compression';
+
+const compressFilter = /\.(js|mjs|json|css|html|txt|svg|xml)$/i;
+
 /**
  * @type {import('vite').UserConfig}
  */
@@ -5,6 +9,14 @@ const config = {
   server: {
     host: '0.0.0.0',
   },
+  plugins: [
+    viteCompression({ algorithm: 'gzip', ext: '.gz', filter: compressFilter }),
+    viteCompression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+      filter: compressFilter,
+    }),
+  ],
 };
 
 export default config;
